df655b76c376821dd687c1964795d284719f2ec6
Jonathan Lau <jon@ssh.wtf>
2020-04-05 21:23:46 +0000

    nt

---
+++ root/quickSort.js
@@ -0,0 +1,34 @@
+
+
+var tB = [2, 5, 1, 9, 10, 3, 4,12,541,23,13,1,51,131,56,34,12,67,45,8,12,34,7,234,0];
+
+var a = [1,2,3,4,5];
+
+/*
+  Divide -> Conquer -> Combine 
+
+
+*/
+
+
+// boundaries of subarray
+const helper = function(arr, start, end) {
+  // base case: leaf worker 
+  if(start === end) {
+    return;
+  }
+  // recursive case: internal node 
+  // index = a random number in range(start, end)
+  // swap A(index), A(start)
+  let pInde = ()
+
+}
+
+const quickSort = function(arr, n) {
+
+
+
+ return arr;
+}
+
+module.exports = {quickSort};
