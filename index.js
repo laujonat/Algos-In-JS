df2ac9240b500e7f9e1715326ba6b06ad2741959
Jonathan <jon@jonathanlau.io>
2020-05-09 02:05:29 -0700

    setup express and webpack

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-"use strict";
-var path = require("path");
-const fs = require("fs");
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-let inputString = "";
-let currentLine = 0;
-let arrayOfFiles = [];
-let dirPath = path.join(__dirname, "lib");
-function getCompiledFiles(dirPath, arrayOfFiles) {
-  let files = fs.readdirSync(dirPath);
-  let self = this;
-  files.forEach(function(file) {
-    arrayOfFiles = arrayOfFiles || [];
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getCompiledFiles(dirPath + "/" + file, arrayOfFiles);
-    } else {
-      arrayOfFiles.push(dirPath + "/" + file);
-    }
-  });
-  return arrayOfFiles;
-}
-getCompiledFiles(dirPath, arrayOfFiles).forEach((filePath) => {
-  require(filePath);
+const app = express();
+app.use(cors()), app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/src/index.html");
 });
-// Reading passed parameter
-process.argv.forEach(function(val, index, array) {
-  console.log(index + ": " + val);
-});
-
-// Getting executable path
-console.log(process.execPath);
-
-console.log(arrayOfFiles);
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = [
+    {
+        name: 'server',
+        mode: 'development',
+        entry: './index.js',
+        watch: true,
+        target: 'node',
+        output: {
+            path: path.resolve('./public'),
+            filename: 'node.js',
+        },
+    },
+    {
+        name: 'client',
+        mode: 'development',
+        watch: true,
+        watchOptions: {
+            aggregateTimeout: 200,
+            poll: 1000
+        },
+        devtool: 'eval',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve('./public'),
+            filename: 'index.js',
+        },
+    }
+];
