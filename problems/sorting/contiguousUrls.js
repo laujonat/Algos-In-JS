04c9b4bfe63136a2bdc2dc12f2faa03e98767162
Jonathan <jon@jonathanlau.io>
2020-07-16 14:21:37 -0700

    wip- contiguous urls

--- problems/sorting/contiguousUrls.js
+++ problems/sorting/contiguousUrls.js
@@ -5,8 +5,31 @@ Write a function that takes two users' browsing histories as input and returns t
 
 For example, given the following two users' histories:
 
-user1 = ['/home', '/register', '/login', '/user', '/one', '/two']
+user1 = ['/home', '/register', .. , .. ,'/login', '/user', '/one', '/two']
 user2 = ['/home', '/red', '/login', '/user', '/one', '/pink']
 You should return the following:
 ['/login', '/user', '/one']
 */
+// n * m
+type H = Array<string>;
+function contiguousUrls(user1: H, user2: H): H {
+  let mlen = 0;
+  let p1 = 0;
+  j;
+  let p2 = 0;
+  let start = 0;
+  let end = 0;
+  // for(let i = 0; i < user1.length; i++) {
+
+  // }
+  // while (p1 < user1.length && p2 < user2.length) {
+  //   if(user1[p1] === user2[p2]) {
+  //     p1++;
+  //     p2++;
+  //   }
+  // }
+}
+var user1 = ["/home", "/login", "/user", "/one", "/two"];
+var user2 = ["/home", "/red", "/login", "/user", "/one", "/pink"];
+console.log(contiguousUrls(user1, user2));
+module.exports = contiguousUrls;
