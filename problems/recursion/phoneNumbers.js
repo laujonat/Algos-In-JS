1ac82582509f2d40e8d505ca17f167e4fd95a812
Jonathan <jon@jonathanlau.io>
2020-09-13 11:38:13 -0700

    phone number backtracking problem

---
+++ problems/recursion/phoneNumbers.js
@@ -0,0 +1,42 @@
+/*
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+*/
+
+function letterCombinations(digits) {
+  if (digits === "") {
+    return [];
+  }
+
+  const phone = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+  };
+
+  let prev = [""];
+  let curr = [];
+  for (let i = 0; i < digits.length; i += 1) {
+    for (let j = 0; j < prev.length; j += 1) {
+      for (let k = 0; k < phone[digits[i]].length; k += 1) {
+        curr.push(prev[j] + phone[digits[i]][k]);
+      }
+    }
+    prev = curr;
+    curr = [];
+  }
+
+  return prev;
+}
+
+module.exports = letterCombinations;
